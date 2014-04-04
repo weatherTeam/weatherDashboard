4876102b508ac94f11da1922ca1bd77cc1a76d75
quentinms <quentinmazarssimon@gmail.com>
2014-04-04 16:28:36 +0200

    Filtering should be ok. Needs more testing (e.g. on 10Gb dataset)

--- wikipedia/WikiFilter.java
+++ wikipedia/WikiFilter.java
@@ -20,16 +20,27 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.mapred.lib.MultipleOutputs;
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
 
+
+/*For a more detailed explanation see wiki: https://github.com/weatherTeam/weatherDashboard/wiki/Wikipedia*/
 public class WikiFilter {
-	
-public static String[] keywords = {"blizzard","cyclone","hurricane", "typhoon" ,"derecho","drought","nor'easter","storm","tornado","wave", "weather event"};
+
+//A list of useful keywords that will define if an article is relevant or not (if the category contains one of those words).
+public static String[] keywords = {"blizzard","cyclone","hurricane", "typhoon" ,"derecho","drought","nor'easter","storm","tornado","heat wave", "cold wave", "weather event"};
+
+//Regex to check if category contains one of the keywords
 public static Pattern categoryIsRelevantRegex = createRegexFromKeywords();
-public static Pattern categoryRegex = Pattern.compile("\\[\\[Category:((\\w|\\s|\\d|')*)(\\|.*)?\\]\\]");	
+
+//Regex to extract category from article
+public static Pattern categoryRegex = Pattern.compile("\\[\\[Category:((\\w|\\s|\\d|')*)(\\|.*)?\\]\\]");
+
+//Regex to extract title from article
 public static Pattern findTitleRegex = Pattern.compile("<title>(.*)</title>");
 
+
+//Create a regex of the form ".*(keyword1|keyword2|...|keywordn).*"
 static Pattern createRegexFromKeywords(){
 	String regex = ".*(";
 	
@@ -42,42 +53,32 @@ static Pattern createRegexFromKeywords(){
 	return Pattern.compile(regex);
 }
 
-static String encodeCategory(String category){
-	category = category.replace(' ', '0');
-	category = category.replace('\'', '0');
-	
-	return category;
-}
-
-
+//Takes an article as an XML fragment as input, output it only if relevant.
 public static class WFMap extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
 		
 		@Override
 		public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
 			String XMLString = value.toString();
 			
-			String articleCategory = "";
-			
-			boolean articleIsRelevant = false;
+		
 
 			Matcher matcherCategory = categoryRegex.matcher(XMLString);
-		    while(matcherCategory.find()&&!articleIsRelevant){
+			
+			//If an article is relevant, we output it.
+		    while(matcherCategory.find()){
 		    	String currentCategory = matcherCategory.group(1);
 		    	currentCategory = currentCategory.trim();
 		   
 		    	Matcher matcherKeyword = categoryIsRelevantRegex.matcher(currentCategory.toLowerCase());
-		    	articleIsRelevant = matcherKeyword.find();
-		    	if(articleIsRelevant){
-		    		articleCategory = matcherKeyword.group(1);
+		    	if(matcherKeyword.find()){
+		    		output.collect(new Text(matcherKeyword.group(1)), new Text(XMLString));
+		    		break;
 		    	}
 		    	
 			 }
-		    
-		    if(!articleCategory.equals("")){
-		    	output.collect(new Text(articleCategory), new Text(XMLString));
-		    }
 			
-			/*if(!articleIsRelevant){
+			/* For debugging purposes
+			  	if(!articleIsRelevant){
 				Matcher matcherTitle = findTitleRegex.matcher(XMLString);
 				matcherTitle.find();
 				System.out.println(matcherTitle.group(1)+": "+categories);
@@ -87,17 +88,11 @@ public static class WFMap extends MapReduceBase implements Mapper<LongWritable, 
 		}
 	}
 
-
-	public static class WFReduce extends MapReduceBase implements Reducer<Text, Text, NullWritable, Text> {
+	//Combine all XML format of articles from a category in a valid XML document
+	public static class WFReduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
 		
-		MultipleOutputs mos = null;
-		
-		public void configure(JobConf job) {
-	        mos = new MultipleOutputs(job);
-	    }
-
 		@Override
-		public void reduce(Text key, Iterator<Text> articles, OutputCollector<NullWritable, Text> output, Reporter reporter) throws IOException {
+		public void reduce(Text key, Iterator<Text> articles, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
 			
 				String XMLFile = "<mediawiki xmlns=\"http://www.mediawiki.org/xml/export-0.8/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.mediawiki.org/xml/export-0.8/ http://www.mediawiki.org/xml/export-0.8.xsd\" version=\"0.8\" xml:lang=\"en\">  <siteinfo>    <sitename>Wikipedia</sitename>    <base>http://en.wikipedia.org/wiki/Main_Page</base>    <generator>MediaWiki 1.23wmf19</generator>    <case>first-letter</case>    <namespaces>      <namespace key=\"-2\" case=\"first-letter\">Media</namespace>      <namespace key=\"-1\" case=\"first-letter\">Special</namespace>      <namespace key=\"0\" case=\"first-letter\" />      <namespace key=\"1\" case=\"first-letter\">Talk</namespace>      <namespace key=\"2\" case=\"first-letter\">User</namespace>      <namespace key=\"3\" case=\"first-letter\">User talk</namespace>      <namespace key=\"4\" case=\"first-letter\">Wikipedia</namespace>      <namespace key=\"5\" case=\"first-letter\">Wikipedia talk</namespace>      <namespace key=\"6\" case=\"first-letter\">File</namespace>      <namespace key=\"7\" case=\"first-letter\">File talk</namespace>      <namespace key=\"8\" case=\"first-letter\">MediaWiki</namespace>      <namespace key=\"9\" case=\"first-letter\">MediaWiki talk</namespace>      <namespace key=\"10\" case=\"first-letter\">Template</namespace>      <namespace key=\"11\" case=\"first-letter\">Template talk</namespace>      <namespace key=\"12\" case=\"first-letter\">Help</namespace>      <namespace key=\"13\" case=\"first-letter\">Help talk</namespace>      <namespace key=\"14\" case=\"first-letter\">Category</namespace>      <namespace key=\"15\" case=\"first-letter\">Category talk</namespace>      <namespace key=\"100\" case=\"first-letter\">Portal</namespace>      <namespace key=\"101\" case=\"first-letter\">Portal talk</namespace>      <namespace key=\"108\" case=\"first-letter\">Book</namespace>      <namespace key=\"109\" case=\"first-letter\">Book talk</namespace>      <namespace key=\"118\" case=\"first-letter\">Draft</namespace>      <namespace key=\"119\" case=\"first-letter\">Draft talk</namespace>      <namespace key=\"446\" case=\"first-letter\">Education Program</namespace>      <namespace key=\"447\" case=\"first-letter\">Education Program talk</namespace>      <namespace key=\"710\" case=\"first-letter\">TimedText</namespace>      <namespace key=\"711\" case=\"first-letter\">TimedText talk</namespace>      <namespace key=\"828\" case=\"first-letter\">Module</namespace>      <namespace key=\"829\" case=\"first-letter\">Module talk</namespace>    </namespaces>  </siteinfo>";
 		
@@ -107,33 +102,38 @@ public static class WFMap extends MapReduceBase implements Mapper<LongWritable, 
 				
 				XMLFile += "</mediawiki>";
 				
-				//mos.getCollector(encodeCategory(key.toString()), reporter).collect(NullWritable.get(), new Text(XMLFile));
-				output.collect(NullWritable.get(), new Text(XMLFile));
+				output.collect(key, new Text(XMLFile));
 
 		}
 
 	}	
 	
-	public static class WFCombine extends MapReduceBase implements Reducer<NullWritable, IntWritable, NullWritable, IntWritable> {
-
+	//Allows us to generate a file for each category. Nicer output. (from https://sites.google.com/site/hadoopandhive/home/how-to-write-output-to-multiple-named-files-in-hadoop-using-multipletextoutputformat)
+	static class MultiFileOutput extends MultipleTextOutputFormat<Text, Text> {
+		
 		@Override
-		public void reduce(NullWritable key, Iterator<IntWritable> values, OutputCollector<NullWritable, IntWritable> output, Reporter reporter) throws IOException {
-			
-
-		}
-
+        protected String generateFileNameForKeyValue(Text key, Text value,String name) {
+                return key.toString()+".xml";
+        }
+        
+        @Override
+        protected Text generateActualKey(Text key, Text value) {
+        	return new Text("");
+        }
 	}
 
 	public static void main(String[] args) throws Exception {
 		
 		
-		boolean local = true;
-		
-		FileSystem fs = FileSystem.get(new Configuration());
-		fs.delete(new Path("output"), true);
+		boolean deleteOutput = true;
+		if(deleteOutput){
+			FileSystem fs = FileSystem.get(new Configuration());
+			fs.delete(new Path(args[1]), true);
+		}
 				
 		JobConf conf = new JobConf(WikiFilter.class);
 		conf.setJobName("Wikipedia filtering");
+		//TODO check how many available on server
 		conf.setNumMapTasks(88);
 		conf.setNumReduceTasks(176);
 
@@ -144,39 +144,15 @@ public static class WFMap extends MapReduceBase implements Mapper<LongWritable, 
 		conf.setReducerClass(WFReduce.class);
 
 		conf.setInputFormat(XMLInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
 		
+		conf.setOutputFormat(MultiFileOutput.class);
 		conf.set("xmlinput.start", "<page>");
 		conf.set("xmlinput.end", "</page>");
 		
-		for(String key: keywords){
-			key = encodeCategory(key);
-			MultipleOutputs.addNamedOutput(conf, key, org.apache.hadoop.mapred.TextOutputFormat.class, NullWritable.class, Text.class);
-		}
-		
-		
-		
-
 		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		
-		if(!local){
-			FileOutputFormat.setOutputPath(conf, new Path("/std022/hw2/tmp"));
-		} else {
-			FileOutputFormat.setOutputPath(conf,  new Path(args[1]));
-		}
+		FileOutputFormat.setOutputPath(conf,  new Path(args[1]));
 
 		JobClient.runJob(conf);
-		
-		
-		/*
-		// We delete the tmp folder
-		FileSystem fs = FileSystem.get(new Configuration());
-		if(local){
-			fs.delete(new Path("tmp"), true);
-		} else {
-			fs.delete(new Path("/std022/hw2/tmp"), true);
-		}
-		*/
 
 	}
 }
