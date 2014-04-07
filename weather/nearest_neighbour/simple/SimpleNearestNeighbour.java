e195bf5c01321098ef3299b4d7140fc2ee26e740
Anders Hennum <hennumjr@gmail.com>
2014-04-07 17:38:33 +0200

    Initial commit of naive implementation of Nearest Neighbour

---
+++ weather/nearest_neighbour/simple/SimpleNearestNeighbour.java
@@ -0,0 +1,126 @@
+package weather.nearest_neighbour.simple;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+public class SimpleNearestNeighbour {
+    
+    public static class NNMapper1 extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
+        
+        private double temp;
+        private String date;
+        
+        public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter reporter) throws IOException {
+            
+            String[] lineArray = value.toString().split(";");
+
+            if (!lineArray[2].equals("NA") ){
+                temp = Double.parseDouble(lineArray[2]);
+                date = lineArray[1];
+                output.collect(new Text(date), new DoubleWritable(temp));
+            }
+        }
+    }
+    
+    public static class NNReducer1 extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {   
+    
+    public static class NNMapper2 extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, Text> {
+        
+        private String temp;
+        private String[] date;
+        private int year;
+        private String month;
+        
+        public void map(LongWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+            
+            String[] lineArray = value.toString().split("\\s+");
+
+            date = lineArray[0].split("\\p{Punct}");
+
+            year = Integer.parseInt(date[1]);
+            month = date[0];
+            temp = lineArray[1];
+            
+            output.collect(new IntWritable(year), new Text(month+";" +temp));
+        }
+    }
+    
+    public static class NNReducer2 extends MapReduceBase implements Reducer<IntWritable, Text, IntWritable, DoubleWritable> {   
+       
+        // Year to compare with. Just hardcoded for now 
+        private double[] year2013 = {-10.683333333333332, -7.933333333333334, -7.883333333333334, -0.6666666666666666, 
+                7.8166666666666655, 10.383333333333333, 13.700000000000001, 11.450000000000001, 7.716666666666666, 2.766666666666667,
+                -2.066666666666667, -2.6142857142857143};
+        private double temp;
+        private int month;
+        private String[] textArray;
+        
+        private double distance; 
+        
+        public void reduce(IntWritable key, Iterator<Text> values, OutputCollector<IntWritable, DoubleWritable> output, Reporter reporter)
+                     throws IOException {
+            
+                distance = 0.0;
+
+                while (values.hasNext()){
+                    System.out.println(values.next());
+                    textArray = values.next().toString().split(";");
+                    month = Integer.parseInt(textArray[0]);
+                    temp = Double.parseDouble(textArray[1]);
+                    
+                    distance += Math.pow(year2013[month-1]-temp, 2);
+                }
+            
+                output.collect(key, new DoubleWritable(distance));
+            }
+        }
+    
+    public static void main(String[] args) throws Exception {
+          JobConf conf1 = new JobConf(NearestNeighbourSimple.class);
+          conf1.setJobName("NN simple phase 1");
+
+          
+          conf1.setOutputKeyClass(Text.class);
+          conf1.setOutputValueClass(DoubleWritable.class);
+
+          conf1.setMapperClass(NNMapper1.class);
+          conf1.setReducerClass(NNReducer1.class);
+
+          conf1.setInputFormat(TextInputFormat.class);
+          conf1.setOutputFormat(TextOutputFormat.class);
+
+          FileInputFormat.setInputPaths(conf1, new Path(args[0]));
+          FileOutputFormat.setOutputPath(conf1, new Path("/tmp/hadoop"));
+          
+          conf1.setNumMapTasks(1);
+          conf1.setNumReduceTasks(1);
+          
+          JobConf conf2 = new JobConf(NearestNeighbourSimple.class);
+          conf2.setJobName("NN simple phase 2");
+          
+          conf2.setOutputKeyClass(IntWritable.class);
+          conf2.setOutputValueClass(Text.class);
+
+          conf2.setMapperClass(NNMapper2.class);
+          conf2.setReducerClass(NNReducer2.class);
+          
+          conf2.setInputFormat(TextInputFormat.class);
+          conf2.setOutputFormat(TextOutputFormat.class);
+
+          FileInputFormat.setInputPaths(conf2, new Path("/tmp/hadoop/part-00000"));
+          FileOutputFormat.setOutputPath(conf2, new Path(args[1]));
+          
+          conf2.setNumMapTasks(1);
+          conf2.setNumReduceTasks(1);
+          
+          // Run jobs
+          JobClient.runJob(conf1);
+          JobClient.runJob(conf2);
+          
+    }
+
+}
