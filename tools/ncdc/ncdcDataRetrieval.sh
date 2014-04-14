f0f6ac139e49e86120fa63d208a13faf4fac5c76
David Sandoz <david.sandoz@sinedolo.ch>
2014-04-14 16:58:00 +0200

    comments split on several lines
    Some comments were too long in a single line. Hence they were split on
    several lines.

--- tools/ncdc/ncdcDataRetrieval.sh
+++ tools/ncdc/ncdcDataRetrieval.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Usage:
-# The first argument is the folder name on the FTP server which is normally the year
-# The second argument is the name of a file listing the files we want to download
+# The first argument is the folder name on the FTP server
+#   which is normally the year
+# The second argument is the name of a file listing
+#   the files we want to download
 
 ### Parameters ###
 year=""
@@ -13,7 +15,8 @@ if [ $# -ne 2 ]; then
     echo "Usage : ncdcDataRetrieval.sh year filelist"
 elif [ -z $1 ] || [ -z $2 ]; then
     echo "No arguments should be null"
-elif [ -s $2 ] && [ -f $2 ] && [ -r $2 ]; then # file is not empty, is not a directory and is readable
+elif [ -s $2 ] && [ -f $2 ] && [ -r $2 ]; then
+    # file is not empty, is not a directory and is readable
     year="$1"
     filename="$2"
     echo "Downloading..."
