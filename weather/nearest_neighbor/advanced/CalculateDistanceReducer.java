e93bd0e014e664649b75cd1e09c53535582b0f61
Anders Hennum <hennumjr@gmail.com>
2014-04-16 18:04:04 +0200

    Removed unused import

--- weather/nearest_neighbor/advanced/CalculateDistanceReducer.java
+++ weather/nearest_neighbor/advanced/CalculateDistanceReducer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
