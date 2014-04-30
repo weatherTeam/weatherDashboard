b1c71fa24e4c0c5d61e8b8421b98f7a8035aea4a
Anders Hennum <hennumjr@gmail.com>
2014-04-30 09:53:47 +0200

    Maps only specified month and calculates average pr station pr day

--- weather/nearest_neighbor/advanced/AverageMonthMapper.java
+++ weather/nearest_neighbor/advanced/AverageMonthMapper.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
@@ -12,7 +13,13 @@ import org.apache.hadoop.mapred.Reporter;
 
 public class AverageMonthMapper extends MapReduceBase implements
 	Mapper<LongWritable, Text, Text, IntWritable> {
+	
 	private static final int MISSING = 9999;
+	private static String referenceMonth;
+	
+	public void configure(JobConf job) {
+		referenceMonth = job.get("referenceMonth");
+	}
 	
 	public void map(LongWritable key, Text value,
 		OutputCollector<Text, IntWritable> output, Reporter reporter)
@@ -25,6 +32,7 @@ public class AverageMonthMapper extends MapReduceBase implements
 	
 	String station = line.substring(4, 10);
 	String month = line.substring(19, 21);
+	String day = line.substring(21,23);
 	int airTemperature;
 	if (line.charAt(87) == '+') {
 		airTemperature = Integer.parseInt(line.substring(88, 92));
@@ -32,8 +40,8 @@ public class AverageMonthMapper extends MapReduceBase implements
 		airTemperature = Integer.parseInt(line.substring(87, 92));
 	}
 	String quality = line.substring(92, 93);
-	if (airTemperature != MISSING && quality.matches("[01459]")) {
-		output.collect(new Text(station+intYear+month), new IntWritable(airTemperature));
+	if (airTemperature != MISSING && quality.matches("[01459]") && referenceMonth.equals(month)) {
+		output.collect(new Text(station+intYear+month+day), new IntWritable(airTemperature));
 	}
 	}
 }
