bf4380580623fdaa21b439bdf1be5930c922bebf
Aubry Cholleton <aubry.cholleton@epfl.ch>
2014-05-04 20:56:21 +0200

    Fix grid coordinates computation. Now always return top-left corner of the cell.

--- weather/temperature/anomalies/grid/Grid.java
+++ weather/temperature/anomalies/grid/Grid.java
@@ -8,24 +8,27 @@ public class Grid {
 		
 		if (EPSG4326mil.charAt(0) == '+') {
 			lat = Double.parseDouble(EPSG4326mil.substring(1,6));
+			lat = lat +  (gridResolutionY-(lat%(gridResolutionY*1000)));
 		} else {
 			lat = Double.parseDouble(EPSG4326mil.substring(0,6));
+			lat = lat - (lat%(gridResolutionY*1000));
 		}
 		
 		if (EPSG4326mil.charAt(6) == '+') {
 			lon = Double.parseDouble(EPSG4326mil.substring(7,13));
+			lon = lon - (lon%(gridResolutionX*1000));
 		} else {
 			lon = Double.parseDouble(EPSG4326mil.substring(6,13));
+			lon = lon + (gridResolutionX - (lon%(gridResolutionX*1000)));
 		}
 		
-		lat = lat - (lat%(gridResolutionY*1000));
-		lon = lon - (lon%(gridResolutionX*1000));
+		//lat = lat - (lat%(gridResolutionY*1000));
+		//lon = lon - (lon%(gridResolutionX*1000));
 		
 		
 		String x = String.format("%+07d", (int)lon);
 		String y= String.format("%+06d", (int)lat); 
-		System.out.println("X:"+x);
-		System.out.println("Y:"+y);
+
 		
 		return y+x;
 	}
