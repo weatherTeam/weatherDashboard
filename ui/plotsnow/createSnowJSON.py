cdd47b98c41709f1182ccef0075fa4f61d715d14
Jonathan Duss <Jo@tsf-444-wpa-3-221.epfl.ch>
2014-05-05 16:24:21 +0200

    python script export 1 json per week

--- ui/plotsnow/createSnowJSON.py
+++ ui/plotsnow/createSnowJSON.py
@@ -1,5 +1,5 @@
 import numpy as np
-import ogr, osr
+import ogr, osr, os.path, string
 #import matplotlib.pyplot as plt
 #from mpl_toolkits.basemap import Basemap
 #from scipy.interpolate import griddata
@@ -7,20 +7,42 @@ import ogr, osr
 from json import dumps, loads
 
 filename = "snow.txt"
-anomalies = np.genfromtxt(filename, dtype = str, usecols=(1, 2, 4), delimiter = '\t')
+anomalies = np.genfromtxt(filename, dtype = str, usecols=(1, 2, 3, 4), delimiter = '\t')
 #stations = np.genfromtxt("stations.txt", dtype = str)
 
-stations = loads('{"type": "FeatureCollection","crs": {"type": "name","properties": {"name": "EPSG:4326"}}, "features": []}')
-outputfile = open('snowcumulation.json', 'w')
+stationsBase = '{"type": "FeatureCollection","crs": {"type": "name","properties": {"name": "EPSG:4326"}}, "features": []}'
+#outputfile = open('snowcumulation.json', 'a')
+lastDate = "nothing"
+date = 'not the same'
 
 for anom in anomalies.tolist() :
 	
-
-	print anom
-
 	lat = float(anom[0])
 	lon = float(anom[1])
-	val = float(anom[2])
+	val = float(anom[3])
+	date = str(anom[2])
+
+
+	if lastDate != date :
+		if 'outputfile' in locals() :
+			outputfile.write(dumps(stations))
+			outputfile.close()
+
+		fileName = "data/snowcumulation-" + date + ".json"
+		if os.path.isfile(fileName) :
+			outputfile = open(fileName, 'r')
+			stations = loads(outputfile.read())
+			outputfile.close()
+			outputfile = open(fileName, 'w')
+		else :
+			outputfile = open(fileName, 'w')
+			stations = loads(stationsBase)
+
+
+
+	#print anom
+
+
 	#lat = int(lat)
 	#lon = int(lon)
 
@@ -45,8 +67,14 @@ for anom in anomalies.tolist() :
 		},
 		"properties": {
 			"value" : val,
+			"time" : date
 		}
 	})
+
+
+
+	lastDate = date
+
 
 outputfile.write(dumps(stations))
 outputfile.close()
