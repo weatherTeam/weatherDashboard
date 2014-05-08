6ca154f839d17599021281f0caf75bf35d2ae38e
Jonathan Duss <Jo@tsf-444-wpa-0-148.epfl.ch>
2014-05-08 10:24:15 +0200

    Create python script to plot monthly average

--- ui/plotsnow/plotAvg.py
+++ ui/plotsnow/plotAvg.py
@@ -1,14 +1,14 @@
 import numpy as np
 import ogr, osr, os.path, string
 import matplotlib.pyplot as plt
-#from mpl_toolkits.basemap import Basemap
-#from scipy.interpolate import griddata
-#from matplotlib.mlab import griddata
+import matplotlib.cm as cm
 from json import dumps, loads
+from matplotlib.pyplot import *
 
 
 filename = "snow_average.txt"
 values = np.genfromtxt(filename, dtype = str, usecols=(0, 1), delimiter = '\t')
+values = np.sort(values, 0)
 #stations = np.genfromtxt("stations.txt", dtype = str)
 
 stationsBase = '{"type": "FeatureCollection","crs": {"type": "name","properties": {"name": "EPSG:4326"}}, "features": []}'
@@ -16,12 +16,20 @@ stationsBase = '{"type": "FeatureCollection","crs": {"type": "name","properties"
 
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
+colors = cm.rainbow(np.linspace(0, 1, 12))
 
-
+all = []
 for m in months :
 	allAvg = []
 	years = []
 	t = []
+	
+	for val in values :
+		date = str(val[0])
+		splitDate = date.split("-")
+		year = splitDate[0]
+		
+	
 
 	for val in values :
 		date = str(val[0])
@@ -36,6 +44,7 @@ for m in months :
 		if month == m :
 			years.append(year)
 			allAvg.append(avg)
+			all.append(allAvg)
 			
 	print m
 	print month
@@ -43,8 +52,11 @@ for m in months :
 	print allAvg
 	plt.figure(1)
 	#plt.subplot(134)
-	plt.plot(years, allAvg, 'bo')
+	plt.plot(years, allAvg, 'r-', color=colors[m-1], label=m)
+	#handles, labels = ax.get_legend_handles_labels()
+	#legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+	
 
-	plt.show()
+plt.show()
 	
 	
