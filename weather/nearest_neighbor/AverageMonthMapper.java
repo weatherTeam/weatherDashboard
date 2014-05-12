d104eea012c981a9df2e903478f979afdab451c8
Anders Hennum <hennumjr@gmail.com>
2014-05-12 13:38:37 +0200

    Added comments and cleand file

--- weather/nearest_neighbor/AverageMonthMapper.java
+++ weather/nearest_neighbor/AverageMonthMapper.java
@@ -25,40 +25,48 @@ public class AverageMonthMapper extends MapReduceBase implements
 		OutputCollector<Text, Text> output, Reporter reporter)
 		throws IOException {
 	
-	String line = value.toString();
-	
-	String year = line.substring(15, 19);
-	int intYear = Integer.parseInt(year);
-	
-	String station = line.substring(4, 10);
-	String month = line.substring(19, 21);
-	
-	// Divide into periods in stead of days
-	int day = Integer.parseInt(line.substring(21,23));
-	//int period = (day-1)/5;
-	//if (day==31){
-	//	period = 5;
-	//}
-	int period = 0;
-	
-	String airTemperature;
-	String precipitation = MISSING; 
+        String line = value.toString();
+        
+        String year = line.substring(15, 19);
+        int intYear = Integer.parseInt(year);
+        
+        String station = line.substring(4, 10);
+        String month = line.substring(19, 21);
+        
+        // Divide month into periods to compare to each other. 
+        // Can also compare day to day averages
+        int day = Integer.parseInt(line.substring(21,23));
+        //int period = (day-1)/5;
+        //if (day==31){
+        //	period = 5;
+        //}
+        
+        // For USA, the region is to big to divide month into periods, thus we
+        // use the average for the whole month, which means only one period
+        int period = 0;
+        
+        String airTemperature;
+        String precipitation = MISSING; 
 
-	if (line.charAt(87) == '+') {
-		airTemperature = line.substring(88, 92);
-	} else {
-		airTemperature = line.substring(87, 92);
-	}
-	
-	int index = line.indexOf("ADDAA1");
-	if (index != -1){
-		precipitation = line.substring(index+8,index+12);
-	}
-	
-	String quality = line.substring(92, 93);
-	if (!airTemperature.equals(MISSING) && quality.matches("[01459]") && referenceMonth.equals(month)) {
-		output.collect(new Text(station+intYear+month+period), 
-				new Text(airTemperature+";"+precipitation));
-	}
+        // Get air temperature
+        if (line.charAt(87) == '+') {
+            airTemperature = line.substring(88, 92);
+        } else {
+            airTemperature = line.substring(87, 92);
+        }
+
+        // Get precipitation    
+        int index = line.indexOf("ADDAA1");
+        if (index != -1){
+            precipitation = line.substring(index+8,index+12);
+        }
+
+        // Emit station, date, temp and precipitation    
+        String quality = line.substring(92, 93);
+        if (!airTemperature.equals(MISSING) && quality.matches("[01459]") 
+                && referenceMonth.equals(month)) {
+            output.collect(new Text(station+intYear+month+period), 
+                    new Text(airTemperature+";"+precipitation));
+        }
 	}
 }
