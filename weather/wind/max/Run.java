5e5b53d95f13bdbef8538aa4a1d989799d3853d6
Cedric Rolland <c.rolland88@gmail.com>
2014-05-20 10:49:01 +0200

    Last modif. Everything is good to merge.

--- weather/wind/max/Run.java
+++ weather/wind/max/Run.java
@@ -109,15 +109,15 @@ public class Run
 		
 
 		// Paths for local run
-		FileOutputFormat.setOutputPath(conf1, new Path("output/1tmpXtrem"));
-
-		FileInputFormat.setInputPaths(conf2, new Path("output/1tmpXtrem"));
-		FileOutputFormat.setOutputPath(conf2, new Path("output/2tmpDateCluster"));
-
-		FileInputFormat.setInputPaths(conf3, new Path("output/2tmpDateCluster"));
-		FileOutputFormat.setOutputPath(conf3, new Path("output/3tmpCluster"));
-		
-		FileInputFormat.setInputPaths(conf4, new Path("output/3tmpCluster"));
+//		FileOutputFormat.setOutputPath(conf1, new Path("output/1tmpXtrem"));
+//
+//		FileInputFormat.setInputPaths(conf2, new Path("output/1tmpXtrem"));
+//		FileOutputFormat.setOutputPath(conf2, new Path("output/2tmpDateCluster"));
+//
+//		FileInputFormat.setInputPaths(conf3, new Path("output/2tmpDateCluster"));
+//		FileOutputFormat.setOutputPath(conf3, new Path("output/3tmpCluster"));
+//		
+//		FileInputFormat.setInputPaths(conf4, new Path("output/3tmpCluster"));
 
 		
 		FileOutputFormat.setOutputPath(conf4, new Path(args[1]));
